1ee08448b99b5b4e417072243e38be4202a01ede
Roger Welsh <rjhwelsh@protonmail.ch>
2018-06-19 00:23:13 +1200

    Allow clients underneath host to receive icmp.

--- ipv4_tables_setup.sh
+++ ipv4_tables_setup.sh
@@ -114,6 +114,10 @@ ${INPUT} -i enp12s0 --protocol tcp --dport 53 -j ACCEPT
 ${OUTPUT} -o enp12s0 --protocol udp --sport 53 -j ACCEPT
 ${OUTPUT} -o enp12s0 --protocol tcp --sport 53 -j ACCEPT
 
+
+# ICMP
+${FORWARD} --protocol icmp --icmp-type echo-reply -j ACCEPT
+
 # All other connections
 
 # Allow incoming, outgoing connections for distcc
