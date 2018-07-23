711afd5c0d12fe38b15335852092397028f542f8
Roger Welsh <rjhwelsh@protonmail.ch>
2018-07-23 23:40:59 +1200

    setup.sh: Tidied.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# This script is a shortcut to starting up scripts based on hostname.
+# Find host file
+HOST_FILE="$(readlink -e `dirname $0`)"/host.d/"$(hostname)"
 
-# Host file
-HOST_FILE="$(readlink -e `dirname $0`)"/host.d/$(hostname)
-
-[[ -x ${HOST_FILE} ]] &&
-	${HOST_FILE} flush &&
-	${HOST_FILE} start || echo "Error! Check '${HOST_FILE}' !"
+[[ -x "$HOST_FILE" ]] &&
+	"${HOST_FILE}" flush &&
+	"${HOST_FILE}" start ||
+		echo "Error! Check '${HOST_FILE}' !"
