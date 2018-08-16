2df2e09c2a183915f5989b65b9423b5b227742ab
Roger Welsh <rjhwelsh@gmail.com>
2018-08-17 10:46:25 +1200

    baleful/address.py: Address and Route base class.
    test/test_address.py: Associated tests.

---
+++ baleful/address.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import ipaddress
+
+
+class Address:
+    """ Provides a base class for handling addresses. """
+    def __init__(self, addr):
+        """ Arguments:
+        addr -- The address string
+        """
+        self.addr = ipaddress.ip_address(addr)
+
+    def __str__(self):
+        return str(self.addr)
+
+
+class Route:
+    """ Provides a base class for handling routes.
+    Routes are a (Source, Destination) address tuple.
+
+    Routes are always described from the perspective of a Client.
+    Hence,
+    Source -- Client Address
+    Destination -- Server Address
+
+    See Topo for how routes are handled to produce an iptables rule.
+    """
+    def __init__(self, src, dst, **kwargs):
+        """Arguments:
+        src -- The source "client" Address
+        dst -- The destination "server" Address
+        kwargs -- Additional rules for route.
+        """
+        self.src = src
+        self.dst = dst
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return '{} -> {}'.format(self.src, self.dst)
---
+++ test/test_address.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+import unittest
+import baleful.address
+
+
+class Test_Address(unittest.TestCase):
+    Address = baleful.address.Address
+
+    def testLoopBackAddr(self):
+        """ Tests basic Address functionality. """
+        lo_addr = self.Address("127.0.0.1")
+        self.assertEqual(str(lo_addr), "127.0.0.1")
+
+
+class Test_Route(unittest.TestCase):
+    Address = baleful.address.Address
+    Route = baleful.address.Route
+
+    def testLoopBackRoute(self):
+        """ Tests loopback Route functionality. """
+        lo_route = self.Route(
+            self.Address("127.0.0.1"),
+            self.Address("127.0.0.1"))
+
+        self.assertEqual(str(lo_route), "127.0.0.1 -> 127.0.0.1")
