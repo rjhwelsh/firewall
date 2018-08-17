1db1f867fafda1888f747691d3e5927171894ead
Roger Welsh <rjhwelsh@gmail.com>
2018-08-17 16:52:07 +1200

    baleful/address.py: Route inherits Rule.

--- baleful/address.py
+++ baleful/address.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
 import ipaddress
+import baleful.rule
 
 
 class Address:
     """ Provides a base class for handling addresses. """
+
     def __init__(self, addr):
         """ Arguments:
         addr -- The address string
         """
-        self.addr = ipaddress.ip_address(addr)
+        if addr:
+            self.addr = ipaddress.ip_address(addr)
+        else:
+            self.addr = str()
 
     def __str__(self):
         return str(self.addr)
 
+    def __bool__(self):
+        return bool(self.addr)
 
-class Route:
+
+class Route(baleful.rule.Rule):
     """ Provides a base class for handling routes.
     Routes are a (Source, Destination) address tuple.
 
@@ -26,23 +34,29 @@ class Route:
 
     See Topo for how routes are handled to produce an iptables rule.
     """
-    def __init__(self, src, dst, **kwargs):
+
+    def __init__(self, src, dst, params=dict(), **kwargs):
         """Arguments:
         src -- The source "client" Address
         dst -- The destination "server" Address
-        kwargs -- Additional rules for route.
+        params -- Additional rule parameters
+        kwargs -- Additional match parameters
         """
 
         # Check src and dst address types match
-        if not isinstance(dst.addr, type(src.addr)):
-            raise(
-                ValueError(
-                    "{} is not an instance of {}".format(
-                        dst.addr, type(src.addr))))
+        if src and dst:
+            if not isinstance(dst.addr, type(src.addr)):
+                raise(
+                    ValueError(
+                        "{} is not an instance of {}".format(
+                            dst.addr, type(src.addr))))
 
         self.src = src
         self.dst = dst
-        self.kwargs = kwargs
+
+        params.update({'src': src,
+                       'dst': dst})
+        super().__init__(params, **kwargs)
 
     def __str__(self):
         return '{} -> {}'.format(self.src, self.dst)
