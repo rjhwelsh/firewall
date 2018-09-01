afd20e48195411ca2d14926df6c76782e35c66e7
Roger Welsh <rjhwelsh@gmail.com>
2018-09-01 18:29:48 +1200

    test/test_topo.py (testMatmul): Added matrix multiplication test.

--- test/test_topo.py
+++ test/test_topo.py
@@ -52,3 +52,30 @@ class Test_Topo(unittest.TestCase):
 
         self.assertEqual(rarr[0].dict(), combo.dict())
         self.assertEqual(rarr[1].dict(), reverse.dict())
+
+    def testMatmul(self):
+        """ Test __matmul__ with RuleArray """
+
+        rule = R.RuleArray(R.Rule(chain="OUTPUT"))
+        rule_reverse = R.RuleArray(R.Rule(chain="INPUT"))
+
+        topo = T.Topology(rule, rule_reverse)
+
+        route = R.Rule(params={'src': '192.168.1.10',
+                               'dst': '192.168.1.1'})
+
+        app = R.Rule(tcp={'dport': 22})
+        app2 = R.Rule(tcp={'dport': 80})
+
+        appArray = R.RuleArray(app, app2)
+
+        combo = route * appArray
+        reverse = combo.copy()
+        for r in reverse:
+            r.reverse()
+
+        rarr = topo @ (route * appArray)
+
+        for c, r in enumerate(combo):
+            self.assertEqual(rarr[2*c+0].dict(), combo[c].dict())
+            self.assertEqual(rarr[2*c+1].dict(), reverse[c].dict())
