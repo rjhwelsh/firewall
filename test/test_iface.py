92f40ab2fa7745d035dc0f08669d9dd5be49879f
Roger Welsh <rjhwelsh@gmail.com>
2018-09-04 21:24:56 +1200

    test/test_iface.py (Rule): Added test.

--- test/test_iface.py
+++ test/test_iface.py
@@ -3,6 +3,8 @@
 # Unit Tests for NetworkInterface
 import unittest
 import baleful.iface
+import netifaces
+import ipaddress
 
 
 class Test_NetworkInterface(unittest.TestCase):
@@ -23,3 +25,28 @@ class Test_NetworkInterface(unittest.TestCase):
         """ Verify raise error on non-existent interface."""
         with self.assertRaises(ValueError):
             self.netif("nosuchinterface")
+
+    def testRule(self):
+        """ Verify rule generation. """
+
+        gw_tuple = netifaces.gateways()[netifaces.AF_INET][0]
+        gw_if = gw_tuple[1]
+        gw_addr = gw_tuple[0]
+
+        addr_tuple = netifaces.ifaddresses(gw_if)[netifaces.AF_INET][0]
+        your_addr = addr_tuple['addr']
+        # netmask = addr_tuple['netmask']
+
+        netif = self.netif(gw_if)
+
+        self.assertGreater(len(netif.rule()), 0,
+                           msg="No local interface found!")
+
+        rarr = netif.rule(chain="OUTPUT", src=1, dst=3)
+
+        self.assertEqual(rarr[0].params['src'],
+                         ipaddress.ip_network(gw_addr))
+        self.assertEqual(rarr[0].params['dst'],
+                         ipaddress.ip_network(your_addr))
+        self.assertEqual(rarr[0].params['out_interface'],
+                         gw_if)
